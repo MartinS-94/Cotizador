31468907e2493ec0364c12136257dd2c8f71fa46
martinsalas <salasauad@gmail.com>
2022-02-01 23:04:17 -0300

    ultima correccion

--- cotizador.js
+++ cotizador.js
@@ -12,7 +12,7 @@ if (document.getElementById("uno").checked){
         resultado = valore / dolar;
         alert("El cambio de Peso argentino a Dolar es: $" + resultado.toFixed(2));
         console.log("------Nueva cotizacion------")
-        console.log("$" + valore + " es igual a " + "$" + resultado.toFixed(2) + " dolares oficiales.");
+        console.log("$" + valore + " pesos argentinos es igual a " + "$" + resultado.toFixed(2) + " dolares oficiales.");
     }
 
     else if (document.getElementById("dos").checked){
@@ -26,28 +26,28 @@ if (document.getElementById("uno").checked){
         resultado = valore / euro;
         alert("El cambio de Peso argentino a Euro es: $" + resultado.toFixed(2));
         console.log("------Nueva cotizacion------")
-        console.log("$" + valore + " es igual a " + "$" + resultado.toFixed(2) + " euros.");
+        console.log("$" + valore + " pesos argentinos es igual a " + "$" + resultado.toFixed(2) + " euros.");
     }
 
     else if (document.getElementById("cuatro").checked){
         resultado = valore / real;
         alert("El cambio de Peso argentino a Real es: $" + resultado.toFixed(2));
         console.log("------Nueva cotizacion------")
-        console.log("$" + valore + " es igual a " + "$" + resultado.toFixed(2) + " reales.");
+        console.log("$" + valore + " pesos argentinos es igual a " + "$" + resultado.toFixed(2) + " reales.");
     }
 
     else if (document.getElementById("cinco").checked){
         resultado = valore / pesoChileno;
         alert("El cambio de Peso argentino a Peso Chileno es: $" + resultado.toFixed(2));
         console.log("------Nueva cotizacion------")
-        console.log("$" + valore + " es igual a " + "$" + resultado.toFixed(2) + " pesos chilenos.");
+        console.log("$" + valore + " pesos argentinos es igual a " + "$" + resultado.toFixed(2) + " pesos chilenos.");
     }
 
     else if (document.getElementById("seis").checked){
         resultado = valore / sol;
         alert("El cambio de Peso argentino a Soles es: $" + resultado.toFixed(2));
         console.log("------Nueva cotizacion------")
-        console.log("$" + valore + " es igual a " + "$" + resultado.toFixed(2) + " soles.");
+        console.log("$" + valore + " pesos argentinos es igual a " + "$" + resultado.toFixed(2) + " soles.");
     }
 
     else {
